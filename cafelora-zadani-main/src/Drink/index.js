612eab8b810d1aee7170e7a1900f69b24c583c10
Kristyna Nedvedova <nedvedovakristyna@gmail.com>
2020-11-01 17:42:34 +0100

    Poslední úpravy a funkční zprovoznění

--- cafelora-zadani-main/src/Drink/index.js
+++ cafelora-zadani-main/src/Drink/index.js
@@ -1,1 +1,40 @@
-import { Layer } from '../Layer';
+import './style.css';
+import { filledLayer } from '../Layer';
+
+const drinkInfo = document.querySelector('.drink__info');
+
+export const Drink = (props) => {
+  const drinky = document.createElement('div');
+  drinky.className = 'drink';
+
+  drinky.innerHTML = `
+  <div class="drink__product">
+    <div class="drink__cup">
+      <img src="/assets/cups/${props.id}.png" />
+    </div>
+    <div class="drink__info">
+      <h3>${props.name}</h3>
+    ${filledLayer({ ingredients: props.layers })}
+    </div>
+  </div>
+  <div class="drink__controls">
+    <button class="order-btn">Objednat</button>
+  </div>`;
+
+  const orderbtn = drinky.querySelector('.order-btn');
+  const drinkCup = drinky.querySelector('.drink__cup');
+
+  const toggleButton = () => {
+    drinkCup.classList.toggle('drink__cup--selected');
+    if (props.ordered === false) {
+      orderbtn.textContent = 'Zrušit';
+      props.ordered = true;
+    } else {
+      orderbtn.textContent = 'Objednat';
+      props.ordered = false;
+    }
+  };
+  orderbtn.addEventListener('click', toggleButton);
+
+  return drinky;
+};
--- cafelora-zadani-main/src/index.js
+++ cafelora-zadani-main/src/index.js
@@ -19,41 +19,55 @@ navItems.forEach((nl) => {
 });
 // konec
 
-// 4)Objednávání
-let ordered = false;
 
-const drinkCup = () => {
-  const drinkCupElm = document.querySelector('.drink__cup');
-  drinkCupElm.classList.toggle('drink__cup--selected');
-  const btnCancel = document.querySelector('.order-btn');
-  ordered = !ordered;
-
-  if (ordered) {
-    btnCancel.textContent = 'Zrušit';
-  }else {
-    btnCancel.textContent = 'Objednat';
-  }
-};
-
-const orderButton = document.querySelector('.order-btn');
-orderButton.addEventListener('click', drinkCup);
-// konec
-
-// 5)Ingredience jako komponenty
-const layers = [
+// drinky
+const drinks = [
   {
-    color: '#feeeca',
-    label: 'mléčná pěna',
+    id: 'cappuccino',
+    name: 'Cappuccino',
+    ordered: false,
+    layers: [
+      {
+        color: '#feeeca',
+        label: 'mléčná pěna',
+      },
+      {
+        color: '#fed7b0',
+        label: 'teplé mléko',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
   },
   {
-    color: '#fed7b0',
-    label: 'teplé mléko',
-  },
-  {
-    color: '#613916',
-    label: 'espresso',
+    id: 'romano',
+    name: 'Romano',
+    ordered: false,
+    layers: [
+      {
+        color: '#fbdf5b',
+        label: 'citrón',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
   },
 ];
 
-
+// 9)napojení na backend
+const drinkList = document.querySelector('.drinks-list');
+fetch('http://cafelora.kodim.cz/api/drinks')
+.then((response) => {
+return response.json();
+})
+.then((data) => {
+data.forEach((d) => {
+drinkList.appendChild(Drink(d));
+});
+});
 // konec
+
