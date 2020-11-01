98ddfb3d14d3a713423488c3536936f0bcbdc521
Kristyna Nedvedova <nedvedovakristyna@gmail.com>
2020-11-01 13:51:39 +0100

    Zprovoznění navigace

--- cafelora-zadani-main/src/index.js
+++ cafelora-zadani-main/src/index.js
@@ -2,3 +2,20 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+// 3)Zprovoznění navigace
+const btnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+const navMenu = () => {
+  navElm.classList.toggle('nav-closed');
+};
+
+btnElm.addEventListener ('click', navMenu);
+
+const a = document.querySelectorAll (a);
+for (let i = 0; i < a.length; i += 1) {
+  a [i].addEventListener('click', aClosed);
+}
+// konec
+// 4)Objednávání
+
+
