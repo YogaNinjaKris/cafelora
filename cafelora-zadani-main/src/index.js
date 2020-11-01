9f41db3db23c4ea0ebc2a445dd72064a3c5f9251
Kristyna Nedvedova <nedvedovakristyna@gmail.com>
2020-11-01 14:13:51 +0100

    4) Objednávání funkční

--- cafelora-zadani-main/src/index.js
+++ cafelora-zadani-main/src/index.js
@@ -16,6 +16,24 @@ for (let i = 0; i < a.length; i += 1) {
   a [i].addEventListener('click', aClosed);
 }
 // konec
-// 4)Objednávání
 
+// 4)Objednávání
+let ordered = false;
+
+const drinkCup = () => {
+  const drinkCupElm = document.querySelector('.drink__cup');
+  drinkCupElm.classList.toggle('drink__cup--selected');
+  const btnCancel = document.querySelector('.order-btn');
+  ordered = !ordered;
+
+  if (ordered) {
+    btnCancel.textContent = 'Zrušit';
+  }else {
+    btnCancel.textContent = 'Objednat';
+  }
+};
+
+const orderButton = document.querySelector('.order-btn');
+orderButton.addEventListener('click', drinkCup);
+// konec
 
